88d3a25ae1523d0fe304976a1221b81290b2d23d
Heliomar P. Marques <heliomarpm@gmail.com>
2024-11-30 00:46:00 -0300

    feat: export excel

--- src/app/pages/compare/compare.component.ts
+++ src/app/pages/compare/compare.component.ts
@@ -2,13 +2,17 @@ import { ToastrService } from 'ngx-toastr';
 import { forkJoin } from 'rxjs';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, signal, ViewChild } from '@angular/core';
+import { Component, ViewChild, signal } from '@angular/core';
+
+import { FlowService } from '@app/core/services/flow.service';
+import { JsonType } from '@app/shared/types';
 
 import Comparator from '../../libs/comparator/comparator';
 import { CodeModalComponent, CodeModalType } from '../../shared/components/code-modal';
 import { Block, Comparison } from './compare.model';
-import { FlowService } from '@app/core/services/flow.service';
-import { JsonType } from '@app/shared/types';
+import * as XLSX from 'xlsx';
+
+// const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 
 @Component({
 	selector: 'app-compare',
@@ -22,7 +26,7 @@ export class CompareComponent {
 
 	codeModal: CodeModalType = { title: '', data: '' };
 
-	diff: Comparison = {
+	data: Comparison = {
 		flowName: '',
 		newVersion: '',
 		oldVersion: '',
@@ -41,17 +45,31 @@ export class CompareComponent {
 		error: 'error',
 	};
 
-	icoFlow = signal<string>("flowsheet");
+	icoFlow = signal<string>('flowsheet');
 
 	constructor(
 		protected http: HttpClient,
 		private flowService: FlowService,
-		private toastr: ToastrService
+		private toastr: ToastrService,
 	) {}
 
-	onCompare(flowName: string, oldVersion: string, newVersion: string) {
+	async onCompare(flowName: string, oldVersion: string, newVersion: string) {
 		const nOldVersion = Math.min(Number(oldVersion), Number(newVersion));
 		const nNewVersion = Math.max(Number(oldVersion), Number(newVersion));
+
+		// if (nNewVersion === 0) {
+		// 	console.log('Buscando a versão');
+
+		// 	// Aguarda a versão antes de prosseguir
+		// 	const flowAvi = await firstValueFrom(this.flowService.getFlow(flowName));
+
+		// 	if (!flowAvi.version) {
+		// 		this.toastr.error('Versão não encontrada', 'Erro');
+		// 		return;
+		// 	}
+
+		// 	console.log('flowAvi', flowAvi);
+		// }
 
 		const oldFlow$ = this.flowService.get<JsonType>(flowName, nOldVersion);
 		const newFlow$ = this.flowService.get<JsonType>(flowName, nNewVersion);
@@ -60,7 +78,7 @@ export class CompareComponent {
 			next: ([oldData, newData]) => {
 				try {
 					const compare = new Comparator(oldData, newData);
-					this.diff = compare.runComparison();
+					this.data = compare.runComparison();
 				} catch (err) {
 					if (err instanceof Error) {
 						this.toastr.error(err.message, 'Comparar versões');
@@ -89,14 +107,14 @@ export class CompareComponent {
 
 		this.http.get('/assets/diff-change-color.js', { responseType: 'text' }).subscribe(template => {
 			const script = template
-				.replace('{{flowName}}', this.diff.flowName)
-				.replace('{{oldVersion}}', this.diff.oldVersion)
-				.replace('{{newVersion}}', this.diff.newVersion)
-				.replace('{{deletedBlocks}}', blocks(this.diff.blocks.deleted))
-				.replace('{{recreatedBlocks}}', blocks(this.diff.blocks.recreated))
-				.replace('{{recreatedUpdatedBlocks}}', blocks(this.diff.blocks.recreatedUpdated))
-				.replace('{{updatedBlocks}}', blocks(this.diff.blocks.updated))
-				.replace('{{addedBlocks}}', blocks(this.diff.blocks.added));
+				.replace('{{flowName}}', this.data.flowName)
+				.replace('{{oldVersion}}', this.data.oldVersion)
+				.replace('{{newVersion}}', this.data.newVersion)
+				.replace('{{deletedBlocks}}', blocks(this.data.blocks.deleted))
+				.replace('{{recreatedBlocks}}', blocks(this.data.blocks.recreated))
+				.replace('{{recreatedUpdatedBlocks}}', blocks(this.data.blocks.recreatedUpdated))
+				.replace('{{updatedBlocks}}', blocks(this.data.blocks.updated))
+				.replace('{{addedBlocks}}', blocks(this.data.blocks.added));
 
 			navigator.clipboard
 				.writeText(script)
@@ -109,13 +127,57 @@ export class CompareComponent {
 					this.icoFlow.set(this.icons.error);
 					this.toastr.error('Erro ao copiar para área de transferência', 'Error ao copiar');
 				})
-				.finally(() => setTimeout(() => (this.icoFlow.set(this.icons.flow)), 2000));
+				.finally(() => setTimeout(() => this.icoFlow.set(this.icons.flow), 2000));
 
 			// this.openDiffModal('Script IUBot', script);
 		});
 	}
 
 	jsonResultDiff() {
-		this.openDiffModal('Resultado Comparação', this.diff);
+		this.openDiffModal('Resultado Comparação', this.data);
 	}
+
+	exportToExcel(): void {
+   const flatData = (Object.keys(this.data.blocks) as Array<keyof typeof this.data.blocks>).flatMap((groupKey) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return this.data.blocks[groupKey].map((block: any) => ({
+        activityType: block.activityType,
+        activityId: block.activityId,
+        activityName: block.activityName,
+        group: groupKey,
+				old_activityId: groupKey.includes('recreated') ? block.diff[0].old : null,
+      }));
+    });
+
+		// Converte para uma planilha
+    const worksheet = XLSX.utils.json_to_sheet(flatData);
+    const workbook = XLSX.utils.book_new();
+		const sheetName = (`${this.data.flowName}_${this.data.oldVersion}x${this.data.newVersion}`)
+    XLSX.utils.book_append_sheet(workbook, worksheet, `diff_${this.data.oldVersion}_x_${this.data.newVersion}`);
+
+    // Exporta para arquivo Excel
+    XLSX.writeFile(workbook, `diferenca_${sheetName}.xlsx`);
+
+    // Cria uma nova worksheet
+    // const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(flatData);
+
+    // // Cria um novo workbook e adiciona a worksheet
+    // const workbook: XLSX.WorkBook = {
+    //   Sheets: { 'Sheet1': worksheet },
+    //   SheetNames: ['Sheet1']
+    // };
+
+    // // Converte o workbook para um arquivo Excel
+    // const excelBuffer: any = XLSX.write(workbook, {
+    //   bookType: 'xlsx',
+    //   type: 'array'
+    // });
+
+    // this.saveAsExcelFile(excelBuffer, 'ExportedData');
+  }
+
+  // private saveAsExcelFile(buffer: any, fileName: string): void {
+  //   const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+  //   saveAs(data, `${fileName}.xlsx`);
+  // }
 }
---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,7 @@
+export * from './sidenav/sidenav.component';
+// export * from './sidenav/sidenav-items';
+export * from './table/table.component';
+// export * from './table/table.type';
+export * from './code-modal/code-modal.component';
+export * from './code-modal/code-modal.type';
+export * from './check-list/check-list.component';
