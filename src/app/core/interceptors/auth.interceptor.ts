e60b873664f73d30dac9a01585bfcccd21e42eae
Heliomar P. Marques <heliomarpm@gmail.com>
2024-12-14 20:48:02 -0300

    feat: wip token & auth service

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {	HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SKIP_TOKEN, TokenService } from '../services/token.service';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+	constructor(private tokenService: TokenService) { }
+
+	intercept(req: HttpRequest<unknown>,next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+		// exemplo de uso:
+		// this.http.get('/api/public', {context: new HttpContext().set(SKIP_TOKEN, true)})
+		if (req.context.get<boolean>(SKIP_TOKEN)) {
+			return next.handle(req);
+		}
+
+		// exemplo de uso:
+		// this.http.get('/api/public', {headers: new HttpHeaders().set('X-Skip-Token', 'true')})
+		if (req.headers.has('X-Skip-Token')) {
+			const modifiedReq = req.clone({
+				headers: req.headers.delete('X-Skip-Token'),
+			});
+			return next.handle(modifiedReq);
+		}
+
+		const token = this.tokenService.getToken();
+		if (token) {
+			const authReq = req.clone({
+				setHeaders: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			return next.handle(authReq);
+		}
+
+		return next.handle(req);
+	}
+}
---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpContextToken } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  private tokenSubject = new BehaviorSubject<string | null>(null);
+  private tokenExpirationTime: number | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  getToken(): Observable<string|null> {
+    if (this.isTokenExpired()) {
+      return this.renewToken();
+    }
+    return this.tokenSubject.asObservable();
+  }
+
+  private renewToken(): Observable<string> {
+    return this.http.post<{ token: string; expiresIn: number }>('api/obter/token', {}).pipe(
+      tap(response => {
+				this.tokenSubject.next(response.token);
+        this.tokenExpirationTime = response.expiresIn; // Date.now() + response.expiresIn * 1000;
+      }),
+			map(response => response.token)
+    );
+  }
+
+  private isTokenExpired(): boolean {
+		console.log('isTokenExpired', !this.tokenExpirationTime || Date.now() > this.tokenExpirationTime);
+    return !this.tokenExpirationTime || Date.now() > this.tokenExpirationTime;
+  }
+}
+
+export const SKIP_TOKEN = new HttpContextToken<boolean>(() => false);
